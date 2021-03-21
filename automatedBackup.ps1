06ac0d6e0da4e811430e883322bd83b027ccd78d
Sagar Agola <sagardagola@gmail.com>
2021-03-21 19:33:33 +0530

    updated script to do all build stuff in one script

--- automatedBackup.ps1
+++ automatedBackup.ps1
@@ -1,24 +1,58 @@
 $basePath = "C:\Users\sagar\Desktop"
-$backupFolder = Join-Path -Path $basePath -ChildPath "Backups"
-$publishFolder = Join-Path -Path $basePath -ChildPath "Client"
+$backupFolderName = "Backup"
+$publishFolderName = "Client"
+$backupFolder = Join-Path -Path $basePath -ChildPath $backupFolderName
+$publishFolder = Join-Path -Path $basePath -ChildPath $publishFolderName
 
 $currentBackupFolderName = (Get-Date).ToString("yyyy_MM_dd")
-$currentBackupFolderName = Join-Path -Path $backupFolder -ChildPath $currentBackupFolderName
+$currentBackupFolderPath = Join-Path -Path $backupFolder -ChildPath $currentBackupFolderName
 
-# create root backup folder (for very first time)
+$configFilePath = Join-Path -Path $backupFolder -ChildPath "web.config"
+
+########## root backup folder (for very first time) ##########
 If(!(test-path $backupFolder))
 {
-    New-Item -ItemType Directory -Force -Path $backupFolder
+    Write-Output ""
+    Write-Output "========> Creating Directory - '$backupFolderName'"
+    New-Item -ItemType Directory -Force -Path $backupFolder | Out-Null
 }
 
-# create folder where current backup will go
-If(test-path $currentBackupFolderName)
+########## create publish folder to prevent error on backup stage ##########
+If(!(test-path $publishFolder))
+{
+    Write-Output ""
+    Write-Output "========> Creating Directory - '$publishFolderName'"
+    New-Item -ItemType Directory -Force -Path $publishFolder | Out-Null
+}
+
+########## Current backup folder ##########
+If(test-path $currentBackupFolderPath)
 {
     $currentBackupFolderName = (Get-Date).ToString("yyyy_MM_dd__hh_mm_ss")
-    $currentBackupFolderName = Join-Path -Path $backupFolder -ChildPath $currentBackupFolderName
+    $currentBackupFolderPath = Join-Path -Path $backupFolder -ChildPath $currentBackupFolderName
 }
+Write-Output ""
+Write-Output "========> Creating Directory - '$currentBackupFolderName'"
+New-Item -ItemType Directory -Force -Path $currentBackupFolderPath | Out-Null
 
-New-Item -ItemType Directory -Force -Path $currentBackupFolderName
+########## Backup ##########
+Write-Output ""
+Write-Output "========> START - Backup"
+Get-ChildItem -Path $publishFolder -Recurse | Move-Item -Destination $currentBackupFolderPath
+Write-Output "========> END - Backup"
 
-# take backup to newly created folder
-Get-ChildItem -Path $publishFolder -Recurse -Exclude web.config | Move-Item -Destination $currentBackupFolderName
+########## NPM install ##########
+Write-Output ""
+Write-Output "========> START - Node package installation"
+npm install
+Write-Output "========> END - Node package installation"
+
+########## Build Angular Project ##########
+Write-Output ""
+Write-Output "========> START - Build Angular Project"
+npm run ng -- build --prod --base-href=/client/ --output-path=$publishFolder
+Write-Output "========> END - Build Angular Project"
+
+########## Copy web.config ##########
+Write-Output "========> Copying web.config file"
+Copy-Item $configFilePath -Destination $publishFolder
